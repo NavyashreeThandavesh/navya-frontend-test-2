ef45411dab6351cf5822dce54522a04ac5a67ff6
NavyashreeThandavesh <nthandavesh@qwinix.io>
2018-03-19 17:26:39 +0530

    pushing a task files

---
+++ js/home.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $("abt").click(function(){
+    ("#mydiv").toggle();
+  });
+  });
+$(document).ready(function(){
+  $("button").click(function(){
+    ("#s").toggle();
+  });
+  });
+  
