a12b154f495392875fba4e540d61d2eb7a60e424
NavyashreeThandavesh <nthandavesh@qwinix.io>
2018-03-23 17:24:40 +0530

    some changes i commit

--- js/index.js
+++ js/index.js
@@ -9,4 +9,28 @@ $(document).ready(function(){
         $("#about").hide();
 
     });
+    $("#info").click(function(){
+      $("#about").toggle();
+      
+    });
+    $("#info1").click(function(){
+      $("#about").toggle();
+      
+    });
+    $("#info2").click(function(){
+      $("#about").toggle();
+      
+    });
+
+$(window).scroll(function () {
+ debugger 
+ if ($(this).scrollTop() > 50) { 
+ $('#back-to-top').fadeIn(); 
+ } else { 
+ $('#back-to-top').fadeOut(); } }); // scroll body to 0px on click
+  $('#back-to-top').click(function () { 
+  	$('#back-to-top').tooltip('hide'); 
+  	 $('body,html').animate({ scrollTop: 0 }, 600); return false; 
+  	}); 
+  $('#back-to-top').tooltip('show'); 
 });
